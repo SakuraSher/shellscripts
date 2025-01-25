40d872f6a753465169d790238c68acb943385a53
SakuraSher <amulyashekhar@Amulyas-MacBook-Air.local>
2025-01-26 00:02:59 +0530

    copied jenkins script from official documentation

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -13,7 +13,12 @@ fi
 
 java --version
 
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
-sudo apt-get install -y jenkins
+sudo apt-get install jenkins
+
+echo "Installed Jenkins.."
