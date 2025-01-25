dffcfa88fb8a2942b00644a8c9ca686a108fad3e
SakuraSher <amulyashekhar@Amulyas-MacBook-Air.local>
2025-01-26 01:05:42 +0530

    addded docker

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -13,12 +13,30 @@ fi
 
 java --version
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+jenkins --version > /dev/null 2>&1
+if [ $? -eq 0];then
+    echo "Java is already installed"
+else
+    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+    echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install jenkins
+    sudo systemctl restart jenkins
 
 echo "Installed Jenkins.."
+
+
+
+echo "Installing Docker"
+sudo apt install -y docker.io
+
+#add usergroup so that they can launch docker
+
+sudo su -
+usermod -aG docker ubuntu
+usermod -aG docker jenkins
+
+systemctl restart docker
