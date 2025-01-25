dba5d1bbb1892f8a61df32da5ea80a19cdc25163
SakuraSher <amulyashekhar@Amulyas-MacBook-Air.local>
2025-01-26 00:08:22 +0530

    updated version of java to 17 to support the current version of jenkins

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -7,7 +7,7 @@ java -version > /dev/null 2>&1
 if [ $? -eq 0]; then 
     echo "java is already installed"
 else
-   sudo apt install -y openjdk-11-jdk
+   sudo apt install -y openjdk-17-jdk
 fi
 
 
