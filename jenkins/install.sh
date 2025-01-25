f0c426fb86a196c8de0b2f1dbeeb57e413648341
SakuraSher <amulyashekhar@Amulyas-MacBook-Air.local>
2025-01-25 23:53:23 +0530

    added correct jenkins command

--- jenkins/install.sh
+++ jenkins/install.sh
@@ -15,4 +15,5 @@ java --version
 
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
 sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt install jenkins
+sudo apt-get update
+sudo apt-get install -y jenkins
